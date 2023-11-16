5a5658c60229be3cb2467eb5b100803a8bc8898d
caele <tsm@qlik.com>
2023-11-16 09:00:03 +0100

    chore: rename package

--- scripts/verify-files.sh
+++ scripts/verify-files.sh
@@ -6,15 +6,9 @@ set -o errexit
 
 echo "Verifying built file count"
 
-while read line; do
-    if [[ $line =~ ^\"name\": ]]; then
-        name=${line#*: \"}
-        name=${name%\"*}
-    fi
-done < package.json
 
 expected_file_count=$(($(find dist -type f | wc -l)-1))
-zip_file_count=$(zipinfo dist/${name}_${VERSION}.zip | grep ^- | wc -l)
+zip_file_count=$(zipinfo dist/qlik-variance-waterfall_${VERSION}.zip | grep ^- | wc -l)
 
 if [ "${expected_file_count}" -ne "${zip_file_count}" ]; then
     # File count is incorrect
